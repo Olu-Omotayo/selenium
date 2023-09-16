968b7a72cfb92bf7f3b79b7617edb027bab3b9bc
Martins Olu-Omotayo <138247338+motayor@users.noreply.github.com>
2023-09-16 09:47:37 -0400

    Update Using_Xpaths.java

--- Using_Xpaths.java
+++ Using_Xpaths.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Using_Xpaths {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.gecko.driver", "/Users/oluswagger/Oluswagger Documents/_Cloud Learnings/Intellipaat/Java_Selenium_Eclipse/drivers/geckodriver");
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		//WebDriverManager.firefoxdriver().setup();
+		 WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com/");
 		
 
