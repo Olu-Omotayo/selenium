73d0157c86c820d1a39372aa91384a11f2289cf9
Martins Olu-Omotayo <138247338+motayor@users.noreply.github.com>
2023-09-16 09:36:36 -0400

    Update Radio_buttons.java

--- Radio_buttons.java
+++ Radio_buttons.java
@@ -12,9 +12,10 @@ public class Radio_buttons {
 
 	public static void main(String[] args) throws InterruptedException 
 	{
-		System.setProperty("webdriver.gecko.driver", "/Users/oluswagger/Oluswagger Documents/_Cloud Learnings/Intellipaat/Java_Selenium_Eclipse/drivers/geckodriver");
-		
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		//WebDriverManager.firefoxdriver().setup();
+		 driver = new ChromeDriver();
+		//driver = new FirefoxDriver();
 		
 		driver.get("https://www.facebook.com/");
 				
