55ef9d13cec11b897c23eba37ec4f266621c89cd
Martins Olu-Omotayo <138247338+motayor@users.noreply.github.com>
2023-09-16 09:05:17 -0400

    Update Launch2.java

--- Launch2.java
+++ Launch2.java
@@ -11,12 +11,8 @@ public class Launch2 {
 
 	public static void main(String[] args) 
 	{
-//		System.setProperty("webdriver.chrome.driver", "/Users/oluswagger/Oluswagger Documents/_Cloud Learnings/Intellipaat/Java_Selenium_Eclipse/drivers/chromedriver");
-		
-		System.setProperty("webdriver.gecko.driver", "/Users/oluswagger/Oluswagger Documents/_Cloud Learnings/Intellipaat/Java_Selenium_Eclipse/drivers/geckodriver");
-		
+		WebDriverManager.chromedriver().setup();
 //		WebDriver driver = new ChromeDriver();
-		
 		WebDriver driver = new FirefoxDriver();
 		
 		driver.get("https://www.facebook.com/");
