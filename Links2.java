944290f541b0161753470f7ee7fa7c5b83aa06c8
Martins Olu-Omotayo <138247338+motayor@users.noreply.github.com>
2023-09-16 09:11:49 -0400

    Update Links2.java

--- Links2.java
+++ Links2.java
@@ -7,8 +7,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Links2 {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.gecko.driver", "/Users/oluswagger/Oluswagger Documents/_Cloud Learnings/Intellipaat/Java_Selenium_Eclipse/drivers/geckodriver");
-		WebDriver driver = new FirefoxDriver();
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.facebook.com/");
 		
 		// find "create new account" menu
